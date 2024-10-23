1c4c4aabd5b6eb4d09c5b66b95dc8b1887394547
Simon Nielsen <simonrn@gmail.com>
2024-10-23 10:48:49 +0200

    Øvelse 1

---
+++ Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Oevelse_3
+{
+    internal class Enemy
+    {
+        private string name;
+
+        public Enemy (string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get => name; set => name = value; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,31 @@ namespace OOP_Oevelse_3
     {
         static void Main(string[] args)
         {
-            
+            Random random = new Random();
+            List<Enemy> enemies = new List<Enemy>();
+            for (int i = 0; i < 10; i++)
+            {
+                enemies.Add(new Enemy($"Enemy{i}"));
+            }
+            foreach (var enemy in enemies)
+            {
+                Console.WriteLine(enemy.Name);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                Console.WriteLine(enemies[i].Name);
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                enemies.RemoveAt(random.Next(0, enemies.Count));
+            }
+            Console.WriteLine();
+            foreach (var enemy in enemies)
+            {
+                Console.WriteLine(enemy.Name);
+            }
+            Console.ReadKey();
         }
     }
 }
